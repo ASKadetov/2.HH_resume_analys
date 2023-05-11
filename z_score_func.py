22cd24351a3ea9df572eb8de6e74709fe6c8b2fc
ASKadetov <kadetov93@gmail.com>
2023-05-11 09:43:41 +0800

    first commit

---
+++ z_score_func.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def outliers_z_score_mod(data, feature, log_scale=False, left=3, right=3):
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    mu = x.mean()
+    sigma = x.std()
+    lower_bound = mu - left * sigma
+    upper_bound = mu + right * sigma
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x >= lower_bound) & (x <= upper_bound)]
+    return outliers, cleaned
